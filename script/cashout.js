cec3616dca6d998a56f4e91f7f2334a77cf68529
OMIT HASAN <omithasan68479@gmail.com>
2025-02-28 01:29:31 +0600

    add cashout feature

--- script/cashout.js
+++ script/cashout.js
@@ -1,16 +1,16 @@
-document.getElementById("addmoney-btn").addEventListener("click", function(event){
+document.getElementById("cashout-btn").addEventListener("click", function(event){
     event.preventDefault();
-    const amount = document.getElementById("addmoney-amount").value;
+    const amount = document.getElementById("cashout-amount").value;
     const convertedAmount = parseFloat(amount);
-    const pin = document.getElementById("addmoney-pin").value;
+    const pin = document.getElementById("cashout-pin").value;
     const convertedPin = parseInt(pin);
     const mainBalance = document.getElementById("main-balance").innerText;
     const convertedMainBalance = parseFloat(mainBalance);
     if(amount > 0 && pin){
         if(convertedPin === 1234){
-            const newBalance = convertedMainBalance + convertedAmount;
+            const newBalance = convertedMainBalance - convertedAmount;
             document.getElementById("main-balance").innerText = newBalance;
-            alert("Money added successfull");
+            alert("Cashout successfull");
         }
         else{
             alert("Pin is invalid");
--- script/toggle.js
+++ script/toggle.js
@@ -3,7 +3,7 @@ document.getElementById("add-money-btn").addEventListener("click", function(){
     document.getElementById("cashout").style.display = "none";
     document.getElementById("add-money").style.display = "block";
 })
-document.getElementById("cashout-btn").addEventListener("click", function(){
+document.getElementById("cash-out-btn").addEventListener("click", function(){
     document.getElementById("cashout").style.display = "block";
     document.getElementById("add-money").style.display = "none";
 })
